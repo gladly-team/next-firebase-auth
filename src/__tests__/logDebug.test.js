7bf9fad6618bf75fc0634f2d72677f3f8f4bf74e
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-09 14:49:00 -0700

    Don't style debug log prefix server-side

--- src/__tests__/logDebug.test.js
+++ src/__tests__/logDebug.test.js
@@ -1,6 +1,9 @@
 /* eslint no-console: 0 */
+jest.mock('src/isClientSide')
 
 beforeEach(() => {
+  const isClientSide = require('src/isClientSide').default
+  isClientSide.mockReturnValue(true)
   jest.spyOn(console, 'log').mockImplementation(jest.fn())
 })
 
@@ -9,20 +12,34 @@ afterEach(() => {
   jest.resetModules()
 })
 
-const prefix = [
+const styledPrefix = [
   '%cnext-firebase-auth',
   'background: #ffa000; color: #fff; border-radius: 2px; padding: 2px 6px',
 ]
 
+const unstyledPrefix = ['next-firebase-auth:']
+
 describe('logDebug', () => {
-  it('logs a message with a prefix when debug logging is enabled', () => {
+  it('logs a message with a styled prefix when debug logging is enabled on the client side', () => {
     expect.assertions(1)
     jest.spyOn(console, 'log').mockImplementation(jest.fn())
     const logDebug = require('src/logDebug').default
     const { setDebugEnabled } = require('src/logDebug')
     setDebugEnabled(true)
     logDebug('hi there')
-    expect(console.log).toHaveBeenCalledWith(...prefix, 'hi there')
+    expect(console.log).toHaveBeenCalledWith(...styledPrefix, 'hi there')
+  })
+
+  it('logs a message with an unstyled prefix when debug logging is enabled on the client side', () => {
+    expect.assertions(1)
+    jest.spyOn(console, 'log').mockImplementation(jest.fn())
+    const isClientSide = require('src/isClientSide').default
+    isClientSide.mockReturnValue(false)
+    const logDebug = require('src/logDebug').default
+    const { setDebugEnabled } = require('src/logDebug')
+    setDebugEnabled(true)
+    logDebug('hi there')
+    expect(console.log).toHaveBeenCalledWith(...unstyledPrefix, 'hi there')
   })
 
   it('does not log anything when debug logging is not enabled', () => {
@@ -42,8 +59,13 @@ describe('logDebug', () => {
     const { setDebugEnabled } = require('src/logDebug')
     setDebugEnabled(true)
     logDebug('hi there', [1, 2, 3], { foo: 'bar' })
-    expect(console.log).toHaveBeenCalledWith(...prefix, 'hi there', [1, 2, 3], {
-      foo: 'bar',
-    })
+    expect(console.log).toHaveBeenCalledWith(
+      ...styledPrefix,
+      'hi there',
+      [1, 2, 3],
+      {
+        foo: 'bar',
+      }
+    )
   })
 })
--- src/logDebug.js
+++ src/logDebug.js
@@ -1,13 +1,19 @@
+import isClientSide from 'src/isClientSide'
+
 let isDebugEnabled = false
 
 const logDebug = (...args) => {
   if (!isDebugEnabled) {
     return
   }
-  const prefix = [
-    '%cnext-firebase-auth',
-    'background: #ffa000; color: #fff; border-radius: 2px; padding: 2px 6px',
-  ]
+
+  // Only add the styled prefix in a browser context.
+  const prefix = isClientSide()
+    ? [
+        '%cnext-firebase-auth',
+        'background: #ffa000; color: #fff; border-radius: 2px; padding: 2px 6px',
+      ]
+    : ['next-firebase-auth:']
 
   // eslint-disable-next-line no-console
   console.log(...prefix, ...args)
