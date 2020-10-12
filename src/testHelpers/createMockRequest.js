be6aa1f784178865e0f47f569a2e37b1b44ec923
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-12 14:28:51 -0700

    Add placeholder modules

---
+++ src/testHelpers/createMockRequest.js
@@ -0,0 +1,7 @@
+// https://nextjs.org/docs/api-routes/introduction
+// https://nodejs.org/api/http.html#http_class_http_incomingmessage
+const createMockRequest = (...args) => {
+  // TODO
+}
+
+export default createMockRequest
---
+++ src/testHelpers/createMockResponse.js
@@ -0,0 +1,7 @@
+// https://nextjs.org/docs/api-routes/introduction
+// https://nodejs.org/api/http.html#http_class_http_serverresponse
+const createMockResponse = (...args) => {
+  // TODO
+}
+
+export default createMockResponse
