a7b56dc0a16c2436e84308d50ded6d449c775591
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-13 14:36:00 -0700

    Disable broken test for now

--- src/__tests__/cookies.test.js
+++ src/__tests__/cookies.test.js
@@ -45,15 +45,18 @@ describe('cookies.js: getCookie', () => {
   })
 })
 
-describe('cookies.js: setCookie', () => {
-  // FIXME:
-  // "res.getHeader is not a function"
-  it('sets the expected cookie value', () => {
-    expect.assertions(0)
-    const mockReq = getMockReq()
-    const mockRes = getMockRes()
-    const { setCookie } = require('src/cookies')
-    setCookie('myThing', 'the-value', { req: mockReq, res: mockRes })
-    // console.log(mockRes.getHeader('set-cookie')) // TODO: find and parse cookie value
-  })
-})
+// TODO
+// eslint-disable-next-line
+// describe('cookies.js: setCookie', () => {
+//   // FIXME:
+//   // "res.getHeader is not a function"
+// eslint-disable-next-line
+//   it('sets the expected cookie value', () => {
+//     expect.assertions(0)
+//     const mockReq = getMockReq()
+//     const mockRes = getMockRes()
+//     const { setCookie } = require('src/cookies')
+//     setCookie('myThing', 'the-value', { req: mockReq, res: mockRes })
+//     // console.log(mockRes.getHeader('set-cookie')) // TODO: find and parse cookie value
+//   })
+// })
