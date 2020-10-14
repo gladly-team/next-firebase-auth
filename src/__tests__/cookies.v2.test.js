a3c72173cc6c9c8746e42799761ed5d00cf433ce
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-14 11:08:56 -0700

    Add explanation

--- src/__tests__/cookies.v2.test.js
+++ src/__tests__/cookies.v2.test.js
@@ -1,3 +1,23 @@
+// Testing Next.js API routes is a little tricky, because there's
+// not an easy way to provide mock request and response objects.
+// https://nextjs.org/docs/api-routes/introduction
+// The request object is a modified version of an IncomingMessage
+// instance:
+// https://nodejs.org/api/http.html#http_class_http_incomingmessage
+// The response object is a modified version of a ServerResponse
+// instance:
+// https://nodejs.org/api/http.html#http_class_http_serverresponse
+// One approach we've used elsewhere is to provide mock `req` and
+// `res` objects, which works for simpler API logic but falls short
+// when there's more complex interaction (e.g., things like cookie
+// middleware) because the mocks might not reflect production
+// behavior.
+// Ideally, Next would provide a guide or helpers to enable simpler
+// testing. Here are a few related discussions:
+// https://github.com/vercel/next.js/discussions/15166
+// https://github.com/vercel/next.js/discussions/17528
+// Meanwhile, here's a project that helps out:
+// https://github.com/Xunnamius/next-test-api-route-handler
 import { testApiHandler } from 'next-test-api-route-handler'
 import { setConfig } from 'src/config'
 import getMockConfig from 'src/testHelpers/getMockConfig'
