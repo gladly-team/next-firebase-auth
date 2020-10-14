f9a4bbb521aa6fd2e3dc5bdfd1023cec34984eb7
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-14 12:38:52 -0700

    Remove unneeded modules

--- src/testHelpers/createMockResponse.js
+++
@@ -1,8 +0,0 @@
-// https://nextjs.org/docs/api-routes/introduction
-// https://nodejs.org/api/http.html#http_class_http_serverresponse
-const createMockResponse = () => {
-  // TODO
-  // Implement ServerResponse and next addons.
-}
-
-export default createMockResponse
