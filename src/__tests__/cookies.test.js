690ec81d9bb6a4db25f97ae678b0092646247ca4
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-14 16:40:14 -0700

    Use options passed to cookies.js

--- src/__tests__/cookies.test.js
+++ src/__tests__/cookies.test.js
@@ -20,8 +20,6 @@
 // https://github.com/Xunnamius/next-test-api-route-handler
 import { testApiHandler } from 'next-test-api-route-handler'
 import setCookieParser from 'set-cookie-parser'
-import { setConfig } from 'src/config'
-import getMockConfig from 'src/testHelpers/getMockConfig'
 import { encodeBase64 } from 'src/encoding'
 
 jest.mock('src/config')
@@ -33,15 +31,22 @@ const parseCookies = (headerVal) => {
   return setCookieParser.parse(splitCookieHeaders)
 }
 
-beforeEach(() => {
-  const mockConfig = getMockConfig()
-  setConfig({
-    ...mockConfig,
-    cookies: {
-      ...mockConfig.cookies,
-      cookieName: 'myNeatApp',
-    },
-  })
+const createGetCookieOptions = () => ({
+  keys: ['some-key', 'another-key'],
+  secure: true,
+  signed: true,
+})
+
+const createSetCookieOptions = () => ({
+  domain: undefined,
+  httpOnly: true,
+  keys: ['some-key', 'another-key'],
+  maxAge: 60 * 60 * 24, // one day
+  overwrite: true,
+  path: '/',
+  sameSite: 'strict',
+  secure: true,
+  signed: true,
 })
 
 describe('cookies.js: getCookie', () => {
@@ -54,7 +59,11 @@ describe('cookies.js: getCookie', () => {
     await testApiHandler({
       handler: async (req, res) => {
         const { getCookie } = require('src/cookies')
-        const cookieVal = getCookie(MOCK_COOKIE_NAME, { req, res })
+        const cookieVal = getCookie(
+          MOCK_COOKIE_NAME,
+          { req, res },
+          { ...createGetCookieOptions(), keys: undefined, signed: false }
+        )
         expect(JSON.parse(cookieVal)).toEqual(MOCK_COOKIE_VAL)
         return res.status(200).end()
       },
@@ -77,7 +86,11 @@ describe('cookies.js: getCookie', () => {
     await testApiHandler({
       handler: async (req, res) => {
         const { getCookie } = require('src/cookies')
-        const cookieVal = getCookie(MOCK_COOKIE_NAME, { req, res })
+        const cookieVal = getCookie(
+          MOCK_COOKIE_NAME,
+          { req, res },
+          { ...createGetCookieOptions(), keys: undefined, signed: false }
+        )
         expect(cookieVal).toBeUndefined()
         return res.status(200).end()
       },
@@ -98,7 +111,11 @@ describe('cookies.js: getCookie', () => {
     await testApiHandler({
       handler: async (req, res) => {
         const { getCookie } = require('src/cookies')
-        const cookieVal = getCookie(MOCK_COOKIE_NAME, { req, res })
+        const cookieVal = getCookie(
+          MOCK_COOKIE_NAME,
+          { req, res },
+          { ...createGetCookieOptions(), keys: undefined, signed: false }
+        )
         expect(cookieVal).toBeUndefined()
         return res.status(200).end()
       },
@@ -118,10 +135,15 @@ describe('cookies.js: setCookie', () => {
     await testApiHandler({
       handler: async (req, res) => {
         const { setCookie } = require('src/cookies')
-        setCookie(MOCK_COOKIE_NAME, MOCK_COOKIE_VALUE, {
-          req,
-          res,
-        })
+        setCookie(
+          MOCK_COOKIE_NAME,
+          MOCK_COOKIE_VALUE,
+          {
+            req,
+            res,
+          },
+          createSetCookieOptions()
+        )
         return res.status(200).end()
       },
       test: async ({ fetch }) => {
@@ -143,14 +165,24 @@ describe('cookies.js: setCookie', () => {
     await testApiHandler({
       handler: async (req, res) => {
         const { setCookie } = require('src/cookies')
-        setCookie('something', 'here', {
-          req,
-          res,
-        })
-        setCookie('foo', 'bar', {
-          req,
-          res,
-        })
+        setCookie(
+          'something',
+          'here',
+          {
+            req,
+            res,
+          },
+          createSetCookieOptions()
+        )
+        setCookie(
+          'foo',
+          'bar',
+          {
+            req,
+            res,
+          },
+          createSetCookieOptions()
+        )
         return res.status(200).end()
       },
       test: async ({ fetch }) => {
--- src/cookies.js
+++ src/cookies.js
@@ -3,27 +3,30 @@ import Cookies from 'cookies'
 import { encodeBase64, decodeBase64 } from 'src/encoding'
 
 const createCookieMgr = ({ req, res }, { keys, secure } = {}) => {
-  if (secure && !keys) {
+  // https://github.com/pillarjs/cookies
+  const cookies = Cookies(req, res, {
+    keys,
+    secure,
+  })
+  return cookies
+}
+
+export const getCookie = (
+  cookieName,
+  { req, res },
+  { keys, secure, ...cookieOptions } = {}
+) => {
+  // TODO: test
+  if (cookieOptions.signed && !keys) {
     throw new Error(
       'The "keys" value must be provided when using signed cookies.'
     )
   }
 
-  // https://github.com/pillarjs/cookies
-  const cookies = Cookies(req, res, {
-    keys,
-    secure: true,
-  })
-  return cookies
-}
-
-export const getCookie = (cookieName, { req, res }, { keys } = {}) => {
-  const cookies = createCookieMgr({ req, res }, { keys })
+  const cookies = createCookieMgr({ req, res }, { keys, secure })
   try {
-    const cookieVal = cookies.get(cookieName, {
-      // FIXME: use user config
-      // signed: true,
-    })
+    // https://github.com/pillarjs/cookies#cookiesget-name--options--
+    const cookieVal = cookies.get(cookieName, cookieOptions)
     return cookieVal ? decodeBase64(cookieVal) : undefined
   } catch (e) {
     // eslint-disable-next-line no-console
@@ -40,18 +43,22 @@ export const setCookie = (
 ) => {
   // TODO: probably cap maxAge to two weeks to enforce security.
 
-  const secure = true // FIXME: use options
-  const FAKE_KEYS = ['hello', 'hi'] // FIXME: use options
+  // TODO: test
+  if (cookieOptions.signed && !keys) {
+    throw new Error(
+      'The "keys" value must be provided when using signed cookies.'
+    )
+  }
 
-  const cookies = createCookieMgr({ req, res }, { keys: FAKE_KEYS, secure }) // FIXME
+  const cookies = createCookieMgr(
+    { req, res },
+    { keys, secure: cookieOptions.secure }
+  )
 
   // If the value is not defined, set the value to undefined
   // so that the cookie will be deleted.
   const valToSet = cookieVal == null ? undefined : encodeBase64(cookieVal)
 
-  // FIXME: use user config
-  cookies.set(cookieName, valToSet, {
-    // https://github.com/pillarjs/cookies#cookiesset-name--value---options--
-    ...cookieOptions,
-  })
+  // https://github.com/pillarjs/cookies#cookiesset-name--value---options--
+  cookies.set(cookieName, valToSet, cookieOptions)
 }
