4ed633f8ebf04f5a3986f81ebad6fe5bbccf18e3
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-15 15:23:23 -0700

    Be more explicit with cookie options

--- src/cookies.js
+++ src/cookies.js
@@ -14,10 +14,10 @@ const createCookieMgr = ({ req, res }, { keys, secure } = {}) => {
 export const getCookie = (
   cookieName,
   { req, res },
-  { keys, secure, ...cookieOptions } = {}
+  { keys, secure, signed } = {}
 ) => {
   // TODO: test
-  if (cookieOptions.signed && !keys) {
+  if (signed && !keys) {
     throw new Error(
       'The "keys" value must be provided when using signed cookies.'
     )
@@ -26,7 +26,7 @@ export const getCookie = (
   const cookies = createCookieMgr({ req, res }, { keys, secure })
   try {
     // https://github.com/pillarjs/cookies#cookiesget-name--options--
-    const cookieVal = cookies.get(cookieName, cookieOptions)
+    const cookieVal = cookies.get(cookieName, { signed })
     return cookieVal ? decodeBase64(cookieVal) : undefined
   } catch (e) {
     // eslint-disable-next-line no-console
@@ -39,26 +39,42 @@ export const setCookie = (
   cookieName,
   cookieVal,
   { req, res },
-  { keys, ...cookieOptions } = {}
+  {
+    keys,
+    domain,
+    httpOnly,
+    maxAge,
+    overwrite,
+    path,
+    sameSite,
+    secure,
+    signed,
+  } = {}
 ) => {
   // TODO: probably cap maxAge to two weeks to enforce security.
 
   // TODO: test
-  if (cookieOptions.signed && !keys) {
+  if (signed && !keys) {
     throw new Error(
       'The "keys" value must be provided when using signed cookies.'
     )
   }
 
-  const cookies = createCookieMgr(
-    { req, res },
-    { keys, secure: cookieOptions.secure }
-  )
+  const cookies = createCookieMgr({ req, res }, { keys, secure })
 
   // If the value is not defined, set the value to undefined
   // so that the cookie will be deleted.
   const valToSet = cookieVal == null ? undefined : encodeBase64(cookieVal)
 
   // https://github.com/pillarjs/cookies#cookiesset-name--value---options--
-  cookies.set(cookieName, valToSet, cookieOptions)
+  cookies.set(cookieName, valToSet, {
+    domain,
+    httpOnly,
+    maxAge,
+    overwrite,
+    path,
+    sameSite,
+    secure,
+    signed,
+  })
 }
