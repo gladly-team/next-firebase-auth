58d25bdb71c102d1688b8beea3d4f6330514ea0a
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-09 12:37:47 -0500

    Add TODOs

--- example/pages/api/login-v2.js
+++ example/pages/api/login-v2.js
@@ -1,5 +1,7 @@
 import { setAuthCookies } from 'next-firebase-auth'
 
+// TODO: change route to /login after we support customizable
+// endpoints.
 const handler = async (req, res) => {
   await setAuthCookies(req, res)
   return res.status(200).json({ status: true })
--- example/pages/api/logout-v2.js
+++ example/pages/api/logout-v2.js
@@ -1,5 +1,7 @@
 import { unsetAuthCookies } from 'next-firebase-auth'
 
+// TODO: change route to /logout after we support customizable
+// endpoints.
 const handler = async (req, res) => {
   await unsetAuthCookies(req, res)
   res.status(200).json({ status: true })
