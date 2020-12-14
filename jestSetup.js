78dcc405f787ff00a80bf89620001c469d543dd8
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-14 11:48:50 -0500

    Add test

--- jestSetup.js
+++ jestSetup.js
@@ -1,3 +1,7 @@
+/* eslint-env jest */
 /* eslint-disable import/no-extraneous-dependencies */
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+
+// `fetch` is polyfilled by Next.js.
+global.fetch = jest.fn()
--- src/__tests__/useFirebaseUser.test.js
+++ src/__tests__/useFirebaseUser.test.js
@@ -1,5 +1,7 @@
-import { renderHook } from '@testing-library/react-hooks'
+import firebase from 'firebase/app'
+import { renderHook, act } from '@testing-library/react-hooks'
 import useFirebaseUser from 'src/useFirebaseUser'
+import { getMockFirebaseUserClientSDK } from 'src/testHelpers/authUserInputs'
 
 jest.mock('firebase/auth')
 jest.mock('firebase/app')
@@ -14,6 +16,26 @@ describe('useFirebaseUser', () => {
     expect(result.current).toEqual({
       user: undefined,
       initialized: false,
+    })
+  })
+
+  it('returns the Firebase user and initialized=true after the Firebase JS SDK has initialized', () => {
+    // Capture the onIdTokenChanged callback.
+    let onIdTokenChangedCallback
+    firebase.auth().onIdTokenChanged.mockImplementation((callback) => {
+      onIdTokenChangedCallback = callback
+      return () => {} // "unsubscribe" function
+    })
+    const mockFirebaseUser = getMockFirebaseUserClientSDK()
+    const { result } = renderHook(() => useFirebaseUser())
+
+    act(() => {
+      // Mock that Firebase calls onIdTokenChanged.
+      onIdTokenChangedCallback(mockFirebaseUser)
+    })
+    expect(result.current).toEqual({
+      user: mockFirebaseUser,
+      initialized: true,
     })
   })
 })
