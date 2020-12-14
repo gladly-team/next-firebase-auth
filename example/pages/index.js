195ba49c4007faa5c9433029ddda5fed0eb77e6e
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-14 13:07:23 -0500

    Add some padding to the example page

--- example/pages/index.js
+++ example/pages/index.js
@@ -6,19 +6,27 @@ import {
 } from 'next-firebase-auth'
 import Header from '../components/Header'
 
+const styles = {
+  content: {
+    padding: 32,
+  },
+}
+
 const Demo = () => {
   const AuthUser = useAuthUser()
   return (
     <div>
       <Header email={AuthUser.email} signOut={AuthUser.signOut} />
-      <p>
-        This page is does not require user auth, so it won't redirect to the
-        login page if you are not signed in.
-      </p>
-      <p>
-        If you remove getServerSideProps from this page, the page will be static
-        and load the authed user on the client side.
-      </p>
+      <div style={styles.content}>
+        <p>
+          This page is does not require user auth, so it won't redirect to the
+          login page if you are not signed in.
+        </p>
+        <p>
+          If you remove getServerSideProps from this page, the page will be
+          static and load the authed user on the client side.
+        </p>
+      </div>
     </div>
   )
 }
