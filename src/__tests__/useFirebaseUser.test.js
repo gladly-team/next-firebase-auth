2fe021f506343da2270c52e69df4ac576a964faa
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-14 12:24:07 -0500

    Add tests

--- src/__tests__/useFirebaseUser.test.js
+++ src/__tests__/useFirebaseUser.test.js
@@ -47,6 +47,53 @@ describe('useFirebaseUser', () => {
     })
   })
 
+  it('calls the login endpoint as expected when the Firebase JS SDK calls `onIdTokenChanged` with an authed user value', async () => {
+    expect.assertions(2)
+    let onIdTokenChangedCallback
+    firebase.auth().onIdTokenChanged.mockImplementation((callback) => {
+      onIdTokenChangedCallback = callback
+      return () => {} // "unsubscribe" function
+    })
+    const mockToken = 'my-token-here'
+    const mockFirebaseUser = {
+      ...getMockFirebaseUserClientSDK(),
+      getIdToken: async () => mockToken,
+    }
+    renderHook(() => useFirebaseUser())
+
+    expect(fetch).not.toHaveBeenCalled()
+    await act(async () => {
+      await onIdTokenChangedCallback(mockFirebaseUser)
+    })
+    expect(fetch).toHaveBeenCalledWith('/api/login-v2', {
+      method: 'POST',
+      headers: {
+        Authorization: mockToken,
+      },
+      credentials: 'include',
+    })
+  })
+
+  it('calls the logout endpoint as expected when the Firebase JS SDK calls `onIdTokenChanged` without an authed user', async () => {
+    expect.assertions(2)
+    let onIdTokenChangedCallback
+    firebase.auth().onIdTokenChanged.mockImplementation((callback) => {
+      onIdTokenChangedCallback = callback
+      return () => {} // "unsubscribe" function
+    })
+    const mockFirebaseUser = undefined
+    renderHook(() => useFirebaseUser())
+
+    expect(fetch).not.toHaveBeenCalled()
+    await act(async () => {
+      await onIdTokenChangedCallback(mockFirebaseUser)
+    })
+    expect(fetch).toHaveBeenCalledWith('/api/logout-v2', {
+      method: 'POST',
+      credentials: 'include',
+    })
+  })
+
   it('unsubscribes from the Firebase `onIdTokenChanged` event when it unmounts', () => {
     expect.assertions(2)
     const onIdTokenChangedUnsubscribe = jest.fn()
