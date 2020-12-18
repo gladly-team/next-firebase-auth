3b335a9948c01bdef654c77e1997413b2ef10afc
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-18 11:43:36 -0500

    Add test

--- src/__tests__/firebaseAdmin.test.js
+++ src/__tests__/firebaseAdmin.test.js
@@ -11,4 +11,12 @@ describe('verifyIdToken', () => {
     const response = await verifyIdToken('some-token')
     expect(response.user).toEqual(mockFirebaseUser)
   })
+
+  it('returns the same token when it has not expired', async () => {
+    const { verifyIdToken } = require('src/firebaseAdmin')
+    const mockFirebaseUser = createMockFirebaseUserAdminSDK()
+    admin.auth().verifyIdToken.mockResolvedValue(mockFirebaseUser)
+    const response = await verifyIdToken('some-token')
+    expect(response.token).toEqual('some-token')
+  })
 })
