39b3320494458bd78c13ed6ce9e34b8e8c05d07a
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-21 14:18:39 -0500

    Remove outdated TODOs

--- example/pages/api/logout.js
+++ example/pages/api/logout.js
@@ -1,7 +1,5 @@
 import { unsetAuthCookies } from 'next-firebase-auth'
 
-// TODO: change route to /logout after we support customizable
-// endpoints.
 const handler = async (req, res) => {
   await unsetAuthCookies(req, res)
   res.status(200).json({ status: true })
