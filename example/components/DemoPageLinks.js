11ee0093bb85961073a4ee1a9350b12dcb9418a2
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-21 12:55:48 -0500

    Add an example page using a loader

--- example/components/DemoPageLinks.js
+++ example/components/DemoPageLinks.js
@@ -19,11 +19,16 @@ const DemoPageLinks = () => (
     <h4>Examples</h4>
     <div>
       <Link href="/">
-        <a style={styles.linkAnchor}>Home (no auth required)</a>
+        <a style={styles.linkAnchor}>Home (static, no auth required)</a>
       </Link>
       <Link href="/ssr-auth-required">
         <a style={styles.linkAnchor}>
           Authentication required: server-side redirect
+        </a>
+      </Link>
+      <Link href="/static-auth-required-loader">
+        <a style={styles.linkAnchor}>
+          Authentication required: static page with loader
         </a>
       </Link>
       <Link href="/auth">
---
+++ example/pages/static-auth-required-loader.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useAuthUser, withAuthUser, AuthAction } from 'next-firebase-auth'
+import Header from '../components/Header'
+import DemoPageLinks from '../components/DemoPageLinks'
+import FullPageLoader from '../components/FullPageLoader'
+
+const styles = {
+  content: {
+    padding: 32,
+  },
+  infoTextContainer: {
+    marginBottom: 32,
+  },
+}
+
+const Demo = () => {
+  const AuthUser = useAuthUser()
+  return (
+    <div>
+      <Header email={AuthUser.email} signOut={AuthUser.signOut} />
+      <div style={styles.content}>
+        <div style={styles.infoTextContainer}>
+          <p>
+            This page requires is static but requires authentication. Before the
+            Firebase client SDK initializes, it shows a loader. After
+            initializing, if the user is not authenticated, it client-side
+            redirects to the login page.
+          </p>
+        </div>
+        <DemoPageLinks />
+      </div>
+    </div>
+  )
+}
+
+export default withAuthUser({
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+  LoaderComponent: FullPageLoader,
+})(Demo)
