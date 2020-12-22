da7ba956e478ace81041295b718d36e3b215f675
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-22 15:20:10 -0500

    Update example to use new config structure

--- example/pages/_app.js
+++ example/pages/_app.js
@@ -4,25 +4,23 @@ import '../styles/globals.css'
 import { init as initAuth } from 'next-firebase-auth'
 
 const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000
-const isServerSide = typeof window === 'undefined'
 initAuth({
   debug: false,
   authPageURL: '/auth',
   appPageURL: '/',
   loginAPIEndpoint: '/api/login',
   logoutAPIEndpoint: '/api/logout',
-  // Don't set the Firebase admin config on the client side.
-  ...(isServerSide && {
-    firebaseAdminInitConfig: {
-      credential: {
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // https://stackoverflow.com/a/41044630/1332513
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      },
-      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  firebaseAdminInitConfig: {
+    credential: {
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // https://stackoverflow.com/a/41044630/1332513
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined,
     },
-  }),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  },
   firebaseClientInitConfig: {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -30,22 +28,18 @@ initAuth({
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   },
   cookies: {
-    cookieName: 'ExampleApp',
-    ...(isServerSide && {
-      keys: [
-        process.env.COOKIE_SECRET_CURRENT,
-        process.env.COOKIE_SECRET_PREVIOUS,
-      ],
-    }),
-    cookieOptions: {
-      httpOnly: true,
-      maxAge: TWELVE_DAYS_IN_MS,
-      overwrite: true,
-      path: '/',
-      sameSite: 'strict',
-      secure: process.env.NEXT_PUBLIC_COOKIE_SECURE === 'true',
-      signed: true,
-    },
+    name: 'ExampleApp',
+    keys: [
+      process.env.COOKIE_SECRET_CURRENT,
+      process.env.COOKIE_SECRET_PREVIOUS,
+    ],
+    httpOnly: true,
+    maxAge: TWELVE_DAYS_IN_MS,
+    overwrite: true,
+    path: '/',
+    sameSite: 'strict',
+    secure: process.env.NEXT_PUBLIC_COOKIE_SECURE === 'true',
+    signed: true,
   },
 })
 
