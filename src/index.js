9ccccd87741105f45bccc275a67b292a63ee1f49
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-29 22:37:40 -0500

    Add TODO note

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ const withAuthUser = withAuthUserModule
 
 const useAuthUser = useAuthUserModule
 
+// TODO: when we implement this, note in the documentation
+//   that this will NOT check for token revocation. If the
+//   developer needs verification that the credentials haven't
+//   been revoked, they should always use `withAuthUserTokenSSR`.
 const withAuthUserSSR = () => {
   throw new Error('"withAuthUserSSR" can only be called server-side.')
 }
