75177ba0c487b30f6b11388c54cbe5b47c9b7199
Jesse Anderson <jesse.anderson@sideinc.com>
2021-12-13 11:48:14 -0500

    test: add main tests for new schema on appPageURL and authPageURL

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -205,7 +205,6 @@ describe('index.js: AuthAction', () => {
       RETURN_NULL: 'returnNull',
       REDIRECT_TO_LOGIN: 'redirectToLogin',
       REDIRECT_TO_APP: 'redirectToApp',
-      REDIRECT: 'redirect',
     })
   })
 })
