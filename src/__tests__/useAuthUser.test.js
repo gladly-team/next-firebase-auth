fae518a1828eaffd96d5639275aa2fa4f0f97d53
Kevin Jennison <kevin.jennison1@gmail.com>
2021-12-21 15:27:44 -0500

    Throw if useAuthUser is used without withAuthUser

---
+++ src/__tests__/useAuthUser.test.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('useAuthUser', () => {
+  test('the AuthUser context is created with an undefined default value', () => {
+    expect.assertions(1)
+    jest.spyOn(React, 'createContext')
+    // eslint-disable-next-line no-unused-expressions
+    require('src/useAuthUser').default
+    expect(React.createContext).toHaveBeenCalledWith()
+  })
+
+  test('defining the AuthUser context value changes the useAuthUser value', () => {
+    expect.assertions(1)
+    const { AuthUserContext } = require('src/useAuthUser')
+    const useAuthUser = require('src/useAuthUser').default
+    let childAuthUserVal
+    const TestComp = () => {
+      const authUser = useAuthUser()
+      childAuthUserVal = authUser
+      return null
+    }
+    render(
+      <AuthUserContext.Provider value="some fake value">
+        <TestComp />
+      </AuthUserContext.Provider>
+    )
+    expect(childAuthUserVal).toEqual('some fake value')
+  })
+
+  test('useAuthUser will throw if used without a defined value for AuthUser context', () => {
+    expect.assertions(1)
+    const useAuthUser = require('src/useAuthUser').default
+    const TestComp = () => {
+      useAuthUser() // should throw
+      return null
+    }
+    const err = new Error(
+      'When using `useAuthUser`, the page must be wrapped in `withAuthUser`.'
+    )
+    // Suppress expected error logs.
+    jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(() => {})
+      .mockImplementationOnce(() => {})
+    expect(() => {
+      render(<TestComp />)
+    }).toThrow(err)
+  })
+})
--- src/useAuthUser.js
+++ src/useAuthUser.js
@@ -1,9 +1,18 @@
 import { createContext, useContext } from 'react'
-import createAuthUser from 'src/createAuthUser'
 
-// Defaults to empty AuthUser object.
-export const AuthUserContext = createContext(createAuthUser())
+// Default to undefined. `useAuthUser` will throw if the value is undefined
+// to ensure the developer wraps pages in `withAuthUser`. See:
+// https://github.com/gladly-team/next-firebase-auth/issues/155
+export const AuthUserContext = createContext()
 
-const useAuthUser = () => useContext(AuthUserContext)
+const useAuthUser = () => {
+  const authUser = useContext(AuthUserContext)
+  if (!authUser) {
+    throw new Error(
+      'When using `useAuthUser`, the page must be wrapped in `withAuthUser`.'
+    )
+  }
+  return authUser
+}
 
 export default useAuthUser
