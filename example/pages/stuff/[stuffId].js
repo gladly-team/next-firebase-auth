31720d50869d97d33a205564d8b0b0d571bec409
Kevin Jennison <kevin.jennison1@gmail.com>
2022-02-22 16:04:11 -0500

    Create dynamic page

---
+++ example/pages/stuff/[stuffId].js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from 'next-firebase-auth'
+import { useRouter } from 'next/router'
+import Header from '../../components/Header'
+import DemoPageLinks from '../../components/DemoPageLinks'
+
+const styles = {
+  content: {
+    padding: 32,
+  },
+  infoTextContainer: {
+    marginBottom: 32,
+  },
+}
+
+const StuffPage = () => {
+  const AuthUser = useAuthUser()
+  const router = useRouter()
+  const { stuffId } = router.query
+  return (
+    <div>
+      <Header email={AuthUser.email} signOut={AuthUser.signOut} />
+      <div style={styles.content}>
+        <div style={styles.infoTextContainer}>
+          <h3>Stuff - A dynamic page example</h3>
+          <p>You're looking at this stuff: {stuffId}</p>
+        </div>
+        <DemoPageLinks />
+      </div>
+    </div>
+  )
+}
+
+export default withAuthUser()(StuffPage)
