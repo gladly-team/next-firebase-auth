edf240a2eb59e6e04fc89350d80eb819e813a731
Hegar Garcia <hegargarcia@gmail.com>
2022-07-28 00:10:07 -0500

    refactor: change base files to typescript

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,22 +1,15 @@
 /**
  * Decode a base64 value to an object.
- *
- * @param {String} string
- * @return {Object}
  */
-export const decodeBase64 = (string) => {
+export const decodeBase64 = (string: string): unknown => {
   const body = Buffer.from(string, 'base64').toString('utf8')
   return JSON.parse(body)
 }
 
 /**
  * Encode an object into a base64-encoded JSON string.
- *
- * @param {Object} obj
- * @return {String}
- * @private
  */
-export const encodeBase64 = (obj) => {
+export const encodeBase64 = (obj: unknown): string => {
   const str = JSON.stringify(obj)
   return Buffer.from(str).toString('base64')
 }
