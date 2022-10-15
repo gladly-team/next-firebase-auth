1eb41ea0b355e0facf7508a4780d4df778b748b5
Kevin Jennison <kevin.jennison1@gmail.com>
2022-10-15 13:12:58 -0400

    Create helper

--- codemod/withauthusertokenssr-to-withusertokenssr.js
+++ codemod/withauthusertokenssr-to-withusertokenssr.js
@@ -1,42 +1,8 @@
-// Adapted from:
-// https://github.com/mui/material-ui/blob/master/packages/mui-codemod/src/v5.0.0/fade-rename-alpha.js
+import renameImport from 'codemod/util/renameImport'
 
 const originalAPIName = 'withAuthUserTokenSSR'
 const newAPIName = 'withUserTokenSSR'
 
 export default function transformer(file, api, options) {
-  const { jscodeshift } = api
-  const root = jscodeshift(file.source)
-
-  const printOptions = options.printOptions || {
-    quote: 'single',
-  }
-
-  let importFound = false
-
-  root.find(jscodeshift.ImportDeclaration).forEach((path) => {
-    if (path.node.source.value.match(/^next-firebase-auth$/)) {
-      path.node.specifiers.forEach((specifier) => {
-        if (
-          specifier.type === 'ImportSpecifier' &&
-          specifier.imported.name === originalAPIName
-        ) {
-          // eslint-disable-next-line no-param-reassign
-          specifier.imported = jscodeshift.identifier(newAPIName)
-          importFound = true
-        }
-      })
-    }
-  })
-
-  if (importFound) {
-    return root
-      .find(jscodeshift.CallExpression, { callee: { name: originalAPIName } })
-      .forEach((path) => {
-        // eslint-disable-next-line no-param-reassign
-        path.node.callee.name = newAPIName
-      })
-      .toSource(printOptions)
-  }
-  return file.source
+  return renameImport({ file, api, options }, { originalAPIName, newAPIName })
 }
