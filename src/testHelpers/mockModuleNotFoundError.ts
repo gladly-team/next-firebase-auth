dba11d239e94c5b6b0ecda5ce2811b6fc5eff9ec
Kevin Jennison <kevin.jennison1@gmail.com>
2023-07-01 15:39:14 -0400

    WIP

--- src/testHelpers/mockModuleNotFoundError.ts
+++ src/testHelpers/mockModuleNotFoundError.ts
@@ -1,7 +1,14 @@
-function ModuleNotFoundError({
-  moduleName,
-  callingModule = 'exampleModule.js',
-}) {
+function ModuleNotFoundError(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  this: any,
+  {
+    moduleName,
+    callingModule = 'exampleModule.js',
+  }: {
+    moduleName: string
+    callingModule: string
+  }
+) {
   this.name = 'ModuleNotFoundError'
   this.code = 'MODULE_NOT_FOUND'
   this.message = `Cannot find module '${moduleName}' from '${callingModule}'`
