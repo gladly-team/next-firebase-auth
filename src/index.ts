6e20190ea7f81d48aa6eef11a8796e4a9fc233c5
Kevin Jennison <kevin.jennison1@gmail.com>
2023-12-31 11:39:19 -0500

    Remove client-side check from browser index entrypoint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import 'firebase/app'
 import 'firebase/auth'
 import initFirebaseClientSDK from 'src/initFirebaseClientSDK'
-import isClientSide from 'src/isClientSide'
 import initCommon, { Init } from 'src/initCommon'
 import type { GetUserFromCookies } from './getUserFromCookies'
 import type { SetAuthCookies } from './setAuthCookies'
@@ -15,11 +14,7 @@ export * from 'src/AuthAction'
 
 export const init: Init = (config) => {
   initCommon(config)
-
-  // On the client side, initialize the Firebase JS SDK.
-  if (isClientSide()) {
-    initFirebaseClientSDK()
-  }
+  initFirebaseClientSDK()
 }
 
 export const getUserFromCookies: GetUserFromCookies = () => {
