2c6edfebd08d953efccfee313e18863c0035b520
Kevin Jennison <kevin.jennison1@gmail.com>
2023-12-31 15:22:02 -0500

    Update example app and README to recommend initializing Firebase prior to NFA (#702)
    * Repro
    
    * Additional repro
    
    * Repro a fix of manual init
    
    * Add TODO
    
    * Update example
    
    * Update README

--- example/utils/initAuth.js
+++ example/utils/initAuth.js
@@ -1,10 +1,21 @@
 /* globals window */
+import { initializeApp } from 'firebase/app'
 import { init } from 'next-firebase-auth'
 import absoluteUrl from 'next-absolute-url'
 
 const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000
 
 const initAuth = () => {
+  // Initialize Firebase.
+  const firebaseClientInitConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  }
+  initializeApp(firebaseClientInitConfig)
+
+  // Initialize next-firebase-auth.
   init({
     debug: true,
 
@@ -78,12 +89,7 @@ const initAuth = () => {
       },
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     },
-    firebaseClientInitConfig: {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    },
+    firebaseClientInitConfig,
     cookies: {
       name: 'ExampleApp',
       keys: [
