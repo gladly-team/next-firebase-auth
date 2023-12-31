c46be3a9f75c6b38309706c4fc229557c258448d
Kevin Jennison <kevin.jennison1@gmail.com>
2023-12-31 12:48:52 -0500

    Additional repro

--- example/pages/_app.js
+++ example/pages/_app.js
@@ -1,8 +1,16 @@
+// TODO: remove eslint-disable
+/* eslint-disable no-console */
 import React from 'react'
 import '../styles/globals.css'
+import { getApp, getApps } from 'firebase/app'
 import initAuth from '../utils/initAuth'
 
 initAuth()
+
+// Calling getApp() here reproduces the error of:
+// "Need to provide options, when not being deployed to hosting via source"
+console.log('Firebase apps:', getApp())
+console.log('Firebase apps:', getApps().length)
 
 const MyApp = ({ Component, pageProps }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
